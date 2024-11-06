c91fd064c15b6a9e9d448b0cf0283975384f95e8
shukumar-542 <shukumar542@gmail.com>
2024-11-06 10:06:07 +0600

    added backend base api

--- src/redux/api/baseApi.jsx
+++ src/redux/api/baseApi.jsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://192.168.10.25:7000/',
+    baseUrl: 'http://134.209.35.249:7000/',
     prepareHeaders: (headers) => {
         const token = JSON.parse(localStorage.getItem('token'));
         if (token) {
@@ -14,7 +14,7 @@ const baseQuery = fetchBaseQuery({
 
 
 // const baseQuery = fetchBaseQuery({
-//     baseUrl: 'http://103.161.9.133:7000/',
+//     baseUrl: 'http://134.209.35.249:7000/',
 //     prepareHeaders: (headers, { getState }) => {
 //       const token = getState().auth?.token;
   
@@ -33,4 +33,4 @@ export const baseApi = createApi({
     endpoints: () => ({})
 });
 
-export const imgURL = "http://192.168.10.25:7000/" 
+export const imgURL = "http://134.209.35.249:7000/" 
